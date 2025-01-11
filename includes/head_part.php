358d72cec15541967b6fe44d2f4766fb2c782237
Kalharz <127644672+navee7274@users.noreply.github.com>
2025-01-12 00:19:13 +0530

    Update head_part.php

--- includes/head_part.php
+++ includes/head_part.php
@@ -5,4 +5,4 @@
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" />
 <link rel="preconnect" href="https://cdn.rmulive.com" />
-<link href="/stylesheets/style.css?v=12" rel="stylesheet" />
+<link href="/stylesheets/style.css?v=14" rel="stylesheet" />
